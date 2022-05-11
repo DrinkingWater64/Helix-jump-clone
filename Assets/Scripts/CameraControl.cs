d3e9091e80f348e446df74dc3d7f562ce3dcf4ec
drinkingWater <aamuti64@gmail.com>
2022-05-12 03:28:28 +0600

    won't work on it anymore

---
+++ Assets/Scripts/CameraControl.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraControl: MonoBehaviour
+{
+    public Transform target;
+    private Vector3 offset;
+    public float smoothSpeed;
+    void Start()
+    {
+        offset = transform.position - target.position;
+    }
+    void Update()
+    {
+        Vector3 newPos = Vector3.Lerp(transform.position, offset + target.position, smoothSpeed);
+        transform.position = newPos;
+    }
+}
