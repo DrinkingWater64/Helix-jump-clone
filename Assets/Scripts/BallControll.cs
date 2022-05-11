8ab5ff6ad9aa8dc04d19411c6c98b81110fb3ea4
drinkingWater <aamuti64@gmail.com>
2022-05-12 04:08:38 +0600

    fixed huge jump issue

--- Assets/Scripts/BallControll.cs
+++ Assets/Scripts/BallControll.cs
@@ -6,20 +6,33 @@ public class BallControll : MonoBehaviour
 {
     [SerializeField] Rigidbody _rb;
     [SerializeField] private float _force;
+    public float _deadTime = .3f;
+    private bool _canJump = true;
     // Start is called before the first frame update
     void Start()
     {
-        
+        _canJump = true;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
     }
 
     private void OnCollisionEnter(Collision collision)
     {
-        _rb.AddForce(Vector3.up * _force);    
+        if (_canJump)
+        {
+            _rb.AddForce(Vector3.up * _force);
+            _canJump = false;
+            StartCoroutine(WaitToJump());
+        }
+    }
+
+    IEnumerator WaitToJump()
+    {
+        yield return new WaitForSeconds(_deadTime);
+        _canJump = true;
     }
 }
